0963f0c0492b8337a5a60a36e3cae5b4325034d3
Daniel Beachnau <beachndl@mail.uc.edu>
2025-02-10 23:01:00 -0500

    Completed Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    // Declaring constants
+    private static final int LEGAL_AGE = 21;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Scanner + variable declaration
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        // Get user input
+        System.out.print("Enter your age: ");
+
+        if (in.hasNextInt()) {
+            // Safe to read in an integer
+            age = in.nextInt();
+            in.nextLine(); // Clear the buffer
+
+            // Process and output
+            if (age >= LEGAL_AGE) {
+                System.out.println("You get a wrist band.");
+            }
+        }
     }
 }
